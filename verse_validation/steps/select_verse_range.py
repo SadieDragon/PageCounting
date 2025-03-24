7ddcfdd45b38d59b15a7f135ca1a48b3a90a8cce
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2025-03-24 14:57:56 -0500

    add the book to the endpoints
    SO here I get 2 fun choices.
    
    Either I get to clean up my non-standard abbreviations...
    Or, I can give this the current book from the book iterator.
    
    Frankly?
    I already have the book iterator written for the last GUI attempt.
    I lost the code for my derpy abbreviation rectification in the fire.
    The choice is obvious.

--- verse_validation/steps/select_verse_range.py
+++ verse_validation/steps/select_verse_range.py
@@ -2,14 +2,21 @@
 from verse_validation.utils import query_points
 
 
-def select_verse_range() -> list[str]:
+def select_verse_range(book: str) -> list[str]:
     '''
     Queries what the first and last verse of the current page of the Bible
     are. Assumes the book is known.
 
+    Args:
+        book (str): The current book title.
+
     Returns:
-        (list[str]): The list of verse endpoints
+        (list[str]): The list of verse endpoints.
     '''
     # Grab the verse endpoints
     endpoints = query_points('verse')
-    print(endpoints)  # DEBUG (shuddup flake8)
+
+    # Append the book to the endpoints
+    bookified_endpoints = []
+    for endpoint in endpoints:
+        bookified_endpoints.append(f'{book} {endpoint}')
