e695eb21aa869236948c3c839ab8b28bef08b9cb
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2025-03-25 15:57:55 -0500

    add step three to the init file

--- verse_validation/steps/__init__.py
+++ verse_validation/steps/__init__.py
@@ -2,3 +2,4 @@
 from verse_validation.steps.create_footnotes_per_verse_dict \
     import create_footnotes_per_verse_dict
 from verse_validation.steps.select_verse_range import select_verse_range
+from verse_validation.steps.validate_verses import validate_verses
