cca60a5ea4220e27deb297c3d87a501e73bef142
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2025-03-26 01:26:19 -0500

    added in book iterator
    Also added the pagination to steps init

--- verse_validation/steps/__init__.py
+++ verse_validation/steps/__init__.py
@@ -1,5 +1,6 @@
 
 from verse_validation.steps.create_footnotes_per_verse_dict \
     import create_footnotes_per_verse_dict
+from verse_validation.steps.pagination import Pagination
 from verse_validation.steps.select_verse_range import select_verse_range
 from verse_validation.steps.validate_verses import validate_verses
