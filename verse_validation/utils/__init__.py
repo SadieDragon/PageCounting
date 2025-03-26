a4f07fbef0d032c89d1a98a8eff87ac296cf2388
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2025-03-26 16:29:10 -0500

    remove invalid import query points
    derp

--- verse_validation/utils/__init__.py
+++ verse_validation/utils/__init__.py
@@ -5,4 +5,3 @@ from verse_validation.utils.create_footnote_list \
 from verse_validation.utils.create_list_of_verses \
     import create_list_of_verses
 from verse_validation.utils.letter_iterator import LetterIterator
-from verse_validation.utils.query_points import query_points
