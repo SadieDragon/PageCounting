aea4a95a7124079fe22d0865b696196663a298fc
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2025-03-30 13:51:09 -0500

    remove create_list_of_versees from init
    Double derp!

--- verse_validation/utils/__init__.py
+++ verse_validation/utils/__init__.py
@@ -1,5 +1,3 @@
 
 from verse_validation.utils.book_iterator import BookIterator
-from verse_validation.utils.create_list_of_verses \
-    import create_list_of_verses
 from verse_validation.utils.letter_iterator import LetterIterator
