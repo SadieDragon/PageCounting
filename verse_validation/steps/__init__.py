6d510d273c2883663212bfecc861237a7d6bcb6d
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2025-04-03 15:54:04 -0500

    remove create footnotes per verse dict
    I have a panel for that now

--- verse_validation/steps/__init__.py
+++ verse_validation/steps/__init__.py
@@ -1,5 +1,3 @@
 
-from verse_validation.steps.create_footnotes_per_verse_dict \
-    import CreateFootnotesPerVerseDict
 from verse_validation.steps.pagination import Pagination
 from verse_validation.steps.validate_verses import validate_verses
